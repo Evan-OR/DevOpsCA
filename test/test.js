595370b6e3410290e4df7ba707d35e38b6a46ac3
Evan-OR <tehevster@gmail.com>
2025-03-14 14:32:12 +0000

    added basic tests

---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Basic Test', function () {
+  it('Test to make sure tests are running.', function () {
+    assert.strictEqual(true, true);
+  });
+});
